a81f36d1ebec4241adb8644697f59eee0d238c34
Farros Mufid <farrosmufid.ofid@gmail.com>
2021-06-27 17:33:42 +0700

    Add game over functionality

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -49,11 +49,15 @@ public class GamePanel extends JPanel implements ActionListener {
 
         if (running) {
 
+            /*
+
             for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.setColor(Color.WHITE);
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
             }
+
+            */
 
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
@@ -67,10 +71,18 @@ public class GamePanel extends JPanel implements ActionListener {
                 // body
                 else {
                     g.setColor(new Color(45, 180, 0));
+                    g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 
                 }
             }
+
+            g.setColor(Color.RED);
+            g.setFont(new Font("courier", Font.PLAIN, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("score: " + applesEaten)) / 2, g.getFont().getSize());
+
+
         } else {
             gameOver(g);
         }
@@ -152,6 +164,17 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g) {
+        // score
+        g.setColor(Color.RED);
+        g.setFont(new Font("courier", Font.PLAIN, 40));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("score: " + applesEaten)) / 2, g.getFont().getSize());
+
+        // Game over text
+        g.setColor(Color.RED);
+        g.setFont(new Font("courier", Font.PLAIN, 75));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("game over!!", (SCREEN_WIDTH - metrics2.stringWidth("game over!!")) / 2, SCREEN_HEIGHT / 2);
 
     }
 
